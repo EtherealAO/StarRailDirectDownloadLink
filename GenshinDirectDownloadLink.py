af707fbdf383a62e4980d1d41927e0a6bea2db21
cloudskytian <cloudskytian@users.noreply.github.com>
2021-06-07 13:28:44 +0800

    Add files via upload

--- GenshinDirectDownloadLink.py
+++ GenshinDirectDownloadLink.py
@@ -2,17 +2,29 @@
 import sys
 import json
 import requests
+import collections
 if __name__ == "__main__":
     with open('README.md','a+') as readme:
         readme.seek(0)
         sys.stdout = readme
-        JsonFile=json.loads(readme.read().strip().strip('```'))
-        stringContent=str(requests.get(f'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18').content,'utf-8')
+        stringContent=str(requests.get('https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18').content,'utf-8')
+        jsonFile=json.loads(readme.read().strip().strip('```'),object_pairs_hook=collections.OrderedDict)
+        changed = False
+        pre_download_game=None
+        try:
+            pre_download_game=json.loads(stringContent)['data']['pre_download_game']
+        except:
+            pre_download_game=""
+        if jsonFile['pre_download_game']!=pre_download_game:
+            jsonFile['pre_download_game']=pre_download_game
+            changed=True
         latest=json.loads(stringContent)['data']['game']['latest']
-        deprecated_packages=JsonFile['deprecated_packages']
-        if JsonFile['latest']!=latest:
-            deprecated_packages.append(JsonFile['latest'])
-            JsonFile['latest']=latest
+        if jsonFile['latest']!=latest:
+            deprecated_packages=jsonFile['deprecated_packages']
+            deprecated_packages.append(jsonFile['latest'])
+            jsonFile['latest']=latest
+            changed=True
+        if changed:
             readme.seek(0)
             readme.truncate()
-            print('```\n'+json.dumps(JsonFile,ensure_ascii=False,indent=4,separators=(',',':'))+'\n```')
+            print('```\n'+json.dumps(jsonFile,ensure_ascii=False,indent=4,separators=(',',':'))+'\n```')
