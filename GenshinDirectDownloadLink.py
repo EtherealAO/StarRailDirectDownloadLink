1815d002f886adca86999df8ca3bfaaa4efd0c18
cloudskytian <cloudskytian@users.noreply.github.com>
2023-02-27 11:38:04 +0800

    Update GenshinDirectDownloadLink.py

--- GenshinDirectDownloadLink.py
+++ GenshinDirectDownloadLink.py
@@ -4,27 +4,34 @@ import json
 import requests
 import collections
 if __name__ == "__main__":
-    with open('README.md','a+') as readme:
+    with open('README.md', 'a+') as readme:
         readme.seek(0)
         sys.stdout = readme
-        stringContent=str(requests.get('https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18').content,'utf-8')
-        jsonFile=json.loads(readme.read().strip().strip('```'),object_pairs_hook=collections.OrderedDict)
+        try_flag = True
+        while (try_flag):
+            stringContent = str(
+                requests.get(
+                    'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18').
+                content, 'utf-8')
+            if stringContent != None and stringContent != '':
+                try_flag = False
+        jsonFile = json.loads(readme.read().strip().strip('```'), object_pairs_hook=collections.OrderedDict)
         changed = False
-        pre_download_game=None
+        pre_download_game = None
         try:
-            pre_download_game=json.loads(stringContent)['data']['pre_download_game']
+            pre_download_game = json.loads(stringContent)['data']['pre_download_game']
         except:
-            pre_download_game=""
-        if jsonFile['pre_download_game']!=pre_download_game:
-            jsonFile['pre_download_game']=pre_download_game
-            changed=True
-        latest=json.loads(stringContent)['data']['game']['latest']
-        if jsonFile['latest']!=latest:
-            deprecated_packages=jsonFile['deprecated_packages']
+            pre_download_game = ""
+        if jsonFile['pre_download_game'] != pre_download_game:
+            jsonFile['pre_download_game'] = pre_download_game
+            changed = True
+        latest = json.loads(stringContent)['data']['game']['latest']
+        if jsonFile['latest'] != latest:
+            deprecated_packages = jsonFile['deprecated_packages']
             deprecated_packages.append(jsonFile['latest'])
-            jsonFile['latest']=latest
-            changed=True
+            jsonFile['latest'] = latest
+            changed = True
         if changed:
             readme.seek(0)
             readme.truncate()
-            print('```\n'+json.dumps(jsonFile,ensure_ascii=False,indent=4,separators=(',',':'))+'\n```')
+            print('```\n' + json.dumps(jsonFile, ensure_ascii=False, indent=4, separators=(',', ':')) + '\n```')
