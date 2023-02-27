0436e7e2294dd66da6068a2ccd5aa41cc13f2eef
cloudskytian <cloudskytian@users.noreply.github.com>
2023-02-27 11:50:23 +0800

    Update GenshinDirectDownloadLink.py

--- GenshinDirectDownloadLink.py
+++ GenshinDirectDownloadLink.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
+
 import sys
 import json
 import requests
 import collections
+
 if __name__ == "__main__":
+    genshin_url = 'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18'
     with open('README.md', 'a+') as readme:
         readme.seek(0)
         sys.stdout = readme
         max_retry_times = 5
         retry_times = 0
+        print('try to get genshin json from {}'.format(genshin_url))
         while (retry_times < max_retry_times):
             try:
-                stringContent = str(
-                    requests.get('https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18').content, 'utf-8')
+                stringContent = str(requests.get(genshin_url).content, 'utf-8')
                 if stringContent != None and stringContent != '':
                     retry_times = 5
             except:
                 retry_times = retry_times + 1
+                print('retry times:{}')
+        print('get genshin json successful')
         jsonFile = json.loads(readme.read().strip().strip('```'), object_pairs_hook=collections.OrderedDict)
         changed = False
         pre_download_game = None
