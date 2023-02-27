9d923444a11b0d1511987747b3dd069bca6ebc72
cloudskytian <cloudskytian@users.noreply.github.com>
2023-02-27 14:13:32 +0800

    Create GenshinDirectDownloadLink.py

--- GenshinDirectDownloadLink.py
+++ GenshinDirectDownloadLink.py
@@ -5,45 +5,58 @@ import json
 import requests
 import traceback
 
+
+def get_online_json(url, max_retry_times=5):
+    print('try to get online json')
+    print('url:"{}"\nmax retry times:{}'.format(url, max_retry_times))
+    retry_times = 1
+    while (retry_times < max_retry_times + 1):
+        try:
+            print('try times:{}'.format(retry_times))
+            stringContent = str(requests.get(url).content, 'utf-8')
+            if stringContent != None and stringContent != '':
+                online_json = json.loads(stringContent)
+                retry_times = max_retry_times + 2
+        except:
+            retry_times = retry_times + 1
+    print('get online json successful\n')
+    return online_json
+
+
+def merge_genshin_json(local_json, online_json):
+    print('try to merge genshin json')
+    local_json['pre_download_game'] = online_json['data']['pre_download_game']
+    latest = online_json['data']['game']['latest']
+    if local_json['latest'] != latest:
+        deprecated_packages = local_json['deprecated_packages']
+        deprecated_packages.append(local_json['latest'])
+        local_json['latest'] = latest
+    print('merge genshin json successful\n')
+    return local_json
+
+
+def get_genshin_json():
+    genshin_url = 'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18'
+    online_json = get_online_json(genshin_url)
+    local_path = 'README.md'
+    with open(local_path, 'a+') as local_file:
+        print('try to read local file')
+        local_file.seek(0)
+        try:
+            local_json = json.loads(local_file.read().strip().strip('```'), object_pairs_hook=collections.OrderedDict)
+        except:
+            local_json = {'pre_download_game': '', 'latest': '', 'deprecated_packages': []}
+        genshin_json = merge_genshin_json(local_json, online_json)
+        local_file.seek(0)
+        local_file.truncate()
+        json_str = json.dumps(genshin_json, ensure_ascii=False, indent=4, separators=(',', ':'))
+        print('try to write local file')
+        local_file.write('```\n{}\n```'.format(json_str))
+        print('get genshin json successful')
+
+
 if __name__ == '__main__':
     try:
-        genshin_url = 'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18'
-        with open('README.md', 'a+') as readme:
-            readme.seek(0)
-            max_retry_times = 5
-            retry_times = 0
-            print('try to get genshin json from {}'.format(genshin_url))
-            while (retry_times < max_retry_times):
-                try:
-                    stringContent = str(requests.get(genshin_url).content, 'utf-8')
-                    if stringContent != None and stringContent != '':
-                        retry_times = 10
-                except:
-                    retry_times = retry_times + 1
-                    print('retry times:{}')
-            print('get genshin json successful')
-            try:
-                json_content = json.loads(readme.read().strip().strip('```'), object_pairs_hook=collections.OrderedDict)
-            except:
-                json_content = {}
-            if json_content == {}:
-                json_content = {'pre_download_game': '', 'latest': '', 'deprecated_packages': []}
-            changed = False
-            pre_download_game = None
-            pre_download_game = json.loads(stringContent)['data']['pre_download_game']
-            if json_content['pre_download_game'] != pre_download_game:
-                json_content['pre_download_game'] = pre_download_game
-                changed = True
-            latest = json.loads(stringContent)['data']['game']['latest']
-            if json_content['latest'] != latest:
-                deprecated_packages = json_content['deprecated_packages']
-                deprecated_packages.append(json_content['latest'])
-                json_content['latest'] = latest
-                changed = True
-            if changed:
-                readme.seek(0)
-                readme.truncate()
-                json_str = json.dumps(json_content, ensure_ascii=False, indent=4, separators=(',', ':'))
-                readme.write('```\n{}\n```'.format(json_str))
+        get_genshin_json()
     except:
         print(traceback.format_exc())
