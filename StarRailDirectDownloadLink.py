01e1a47429200c2e46e6713a86ecce37c89a6a38
离披 <lipi@shuise.dev>
2023-04-24 16:23:32 +0800

    starrail init

--- StarRailDirectDownloadLink.py
+++ StarRailDirectDownloadLink.py
@@ -24,22 +24,22 @@ def get_online_json(url, max_retry_times=5):
     return online_json
 
 
-def merge_genshin_json(local_json, online_json):
-    print('try to merge genshin json')
+def merge_starrail_json(local_json, online_json):
+    print('try to merge starrail json')
     local_json['pre_download_game'] = online_json['data']['pre_download_game']
     latest = online_json['data']['game']['latest']
     if local_json['latest'] != latest:
         deprecated_packages = local_json['deprecated_packages']
         deprecated_packages.append(local_json['latest'])
         local_json['latest'] = latest
-    print('merge genshin json successful\n')
+    print('merge starrail json successful\n')
     return local_json
 
 
-def get_genshin_json():
-    print('try to get genshin json\n')
-    genshin_url = 'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18'
-    online_json = get_online_json(genshin_url)
+def get_starrail_json():
+    print('try to get starrail json\n')
+    starrail_url = 'https://api-launcher-static.mihoyo.com/hkrpg_cn/mdk/launcher/api/resource?channel_id=1&key=6KcVuOkbcqjJomjZ&launcher_id=33&sub_channel_id=1'
+    online_json = get_online_json(starrail_url)
     local_path = 'README.md'
     with open(local_path, 'a+') as local_file:
         print('try to read local file')
@@ -48,18 +48,18 @@ def get_genshin_json():
             local_json = json.loads(local_file.read().strip().strip('```'), object_pairs_hook=collections.OrderedDict)
         except:
             local_json = {'pre_download_game': '', 'latest': '', 'deprecated_packages': []}
-        genshin_json = merge_genshin_json(local_json, online_json)
+        starrail_json = merge_starrail_json(local_json, online_json)
         local_file.seek(0)
         local_file.truncate()
-        json_str = json.dumps(genshin_json, ensure_ascii=False, indent=4, separators=(',', ':'))
+        json_str = json.dumps(starrail_json, ensure_ascii=False, indent=4, separators=(',', ':'))
         print('try to write local file')
         local_file.write('```\n{}\n```'.format(json_str))
-        print('get genshin json successful')
+        print('get starrail json successful')
 
 
 if __name__ == '__main__':
     try:
-        t = threading.Thread(target=get_genshin_json)
+        t = threading.Thread(target=get_starrail_json)
         t.daemon = True
         t.start()
         t.join(timeout=300)
